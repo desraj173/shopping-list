a9d508892171daa6ac6c9647f1aba7ea23976687
desraj173 <35986839+desraj173@users.noreply.github.com>
2018-12-27 16:55:20 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb://desraj:desraj123@ds029476.mlab.com:29476/mern_shopping',
+    mongoURI: 'mongodb://<your mongo address>',
     ENV: process.env.NODE_ENV,
     PORT: process.env.PORT || 5000,
 }
